2f8bc51cb3df1440378ef0d961ccb6516aba8728
xdustinface <xdustinfacex@gmail.com>
2026-05-08 13:59:32 +1000

    feat: add `createLLMClient` factory and `parseModelSpec` registry

---
+++ src/providers/index.ts
@@ -0,0 +1,12 @@
+export { AnthropicClient } from './anthropic';
+export { createLLMClient } from './factory';
+export { parseModelSpec } from './model-registry';
+export type { ModelSpec } from './model-registry';
+export type {
+  AnthropicAuth,
+  LLMClient,
+  LLMResponse,
+  ProviderAuth,
+  ProviderName,
+  SendMessageOptions,
+} from './types';
